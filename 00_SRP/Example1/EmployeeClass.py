dd552db3b8989b6e73e8903ff95132acba0974db
Maurice Künicke <m.kuenicke1995@gmail.com>
2022-08-01 20:14:09 +0200

    add srp example1

---
+++ 00_SRP/Example1/EmployeeClass.py
@@ -0,0 +1,73 @@
+# Naive implementation
+class Employee:
+    def __init__(self, name):
+        """
+        Employee Data is stored here
+        """
+        self.name = name
+    
+    def _regular_hours(self):
+        print("A way to calculate non-overtime hours.")
+
+    def calculate_pay(self):
+        x = self._regular_hours()
+        print("Calculating the pay using x.")
+
+    def report_hours(self):
+        x = self._regular_hours()
+        print("Reporting the hours using x.")
+
+    def save(self):
+        print("Save employee data in database.")
+
+
+# Changing the code in the regular_hours() function based on the requirements of one actor,
+# e.g accounting, will result in wrong numbers in the report_hours() function. However, a sufficient
+# test suite might catch the error early on but you still have to fix it while not creating 
+# a mess. So you have to apply changes to the Employee class to change how employees are
+# defined AND to adjust the way regular hours are calcuated. So there would be an additional
+# reason to change the class. 
+
+# Solution: facade implementation pattern
+
+class Employee:
+    def __init__(self, name):
+        """
+        Employee Data is stored here
+        """
+        self.name = name
+
+    def calculate_pay(self):
+        pc = PayCaculator()
+        print("Calculating the pay using pc.")
+
+    def report_hours(self):
+        hr = HourReporter()
+        print("Reporting the hours using hr.")
+
+    def save(self):
+        es = EmployeeSaver()
+        print("Save employee data in database.")
+
+
+class HourReporter:
+    def report_hours(self):
+        rh = RegularHoursCalculator()
+
+class PayCaculator:
+    def calculate_pay(self):
+        rh = RegularHoursCalculator()
+        pass
+
+class EmployeeSaver:
+    def save_employee(self):
+        pass
+
+class RegularHoursCalculator:
+    def regular_hours(self):
+        print("A way to calculate non-overtime hours.")
+
+# While this solution looks more complicated then the original approach, we have to keep in 
+# mind that a lot more functions will be used inside the classes. You probably need a few more
+# functions to calculate the pay for an employee. Now there's only one single reason to change
+# a class.
--- 00_SRP/README.md
+++ 00_SRP/README.md
@@ -1,1 +1,10 @@
 # SRP: The Single Responsibility Principle
+
+```
+A module should be responsible to one, and only one, actor.
+```
+<cite>Robert C.Martin: Clean Architecture</cite>
+
+------------------------------------
+## Example 1
+The first example introduces the Employee class containing three methods: `calculate_pay()`, `report_hours()` and `save()`. Now this alone violates the SRP since all three functions serve a different actor. `calculate_pay()` is defined by accounting, `report_hours()` is defined by operations and `save()` is defined by the database administration. This coupling can cause problems when one actor changes the scope. A possible solution could be the implementation using a facade implementation pattern.
